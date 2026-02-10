a7bd64981d0be7806d1b40990b27bdfff0ba7336
Josh Medeski <josh.medeski@gmail.com>
2026-02-10 16:59:55 -0600

    Always open file to first git hunk

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -64,6 +64,20 @@ return {
         -- Toggles
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
         map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+
+        -- Jump to first hunk on file open
+        local jumped = false
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'GitSignsUpdate',
+          callback = function(args)
+            if jumped or args.buf ~= bufnr then
+              return
+            end
+            jumped = true
+            gitsigns.nav_hunk('first', { wrap = false, navigation_message = false })
+            vim.cmd 'normal! zt'
+          end,
+        })
       end,
     },
   },
