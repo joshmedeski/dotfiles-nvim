12751832e1aa306fafebf7fc5494be45d3dbd56d
Josh Medeski <josh.medeski@gmail.com>
2026-02-11 09:20:40 -0600

    Ensure catppuccin always loads with lazy false

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -10,6 +10,7 @@ Soothing pastel theme
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
+  lazy = false,
   priority = 1000, -- Make sure to load this before all the other start plugins.
 
   dependencies = {
