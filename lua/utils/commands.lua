9da374c083cec2a50917e70075ce5988f0243367
Josh Medeski <josh.medeski@gmail.com>
2026-02-10 22:36:33 -0600

    Fix my commands to use snacks

--- lua/utils/commands.lua
+++ lua/utils/commands.lua
@@ -9,10 +9,10 @@
 
 -- telescope pickers
 vim.cmd [[command! -nargs=0 Commit :Neogit commit]]
-vim.cmd [[command! -nargs=0 GoToCommand :Telescope commands]]
-vim.cmd [[command! -nargs=0 GoToFile :lua vim.defer_fn(function() require('fff').find_files() end, 100)]]
+vim.cmd [[command! -nargs=0 GoToCommand :lua Snacks.picker.command_history()]]
+vim.cmd [[command! -nargs=0 GoToFile :lua Snacks.picker.smart()]]
 vim.cmd [[command! -nargs=0 GoToSymbol :Telescope lsp_document_symbols]]
-vim.cmd [[command! -nargs=0 Grep :Telescope live_grep]]
+vim.cmd [[command! -nargs=0 Grep :lua Snacks.picker.grep()]]
 vim.cmd [[command! -nargs=0 SmartGoTo :Telescope smart_goto]]
 vim.cmd [[command! -nargs=0 Zen :lua Snacks.zen()]]
 vim.cmd [[command! -nargs=0 FindAndReplace :lua require('grug-far').open()]]
