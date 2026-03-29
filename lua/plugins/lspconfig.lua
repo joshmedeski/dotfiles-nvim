373b35919c7576bf1815a7b7e3366edc06fece9f
Josh Medeski <josh.medeski@gmail.com>
2026-03-29 18:49:22 -0500

    Add github actions lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -50,6 +50,28 @@ return {
       'saghen/blink.cmp',
     },
     config = function()
+      -- GitHub Actions language server
+      vim.filetype.add {
+        pattern = {
+          ['.*/%.github/workflows/.*%.ya?ml'] = 'yaml.github',
+        },
+      }
+
+      vim.lsp.config('gh_actions_ls', {
+        cmd = { 'gh-actions-language-server', '--stdio' },
+        filetypes = { 'yaml.github' },
+        root_markers = { '.github' },
+        single_file_support = true,
+        capabilities = {
+          workspace = {
+            didChangeWorkspaceFolders = {
+              dynamicRegistration = true,
+            },
+          },
+        },
+      })
+      vim.lsp.enable 'gh_actions_ls'
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('jam-lsp-attach', { clear = true }),
         callback = function(event)
